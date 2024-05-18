da4c0cf47f7f279526fd332eb9314705d63fba87
Zgrovetin <534477@gmail.com>
2024-05-19 00:00:52 +0200

    Separate router files

--- router.php
+++ router.php
@@ -1,6 +1,10 @@
 <?php
-declare(strict_types=1);
+//declare(strict_types=1);
 
 $routes = require basePath('routes.php');
 
-array_key_exists($uri, $routes) ? require(basePath($routes[$uri])) : http_response_code(404); require(basePath($routes['404']));
+if (array_key_exists($uri, $routes)) {
+    require(basePath($routes[$uri]));
+} else { http_response_code(404);
+    require(basePath($routes['404']));
+}
