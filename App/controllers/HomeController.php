d76abfe5d90e90b9ac707cb6a8e466d2b5b41c8d
Zgrovetin <534477@gmail.com>
2024-05-24 05:35:12 +0200

    Controller classes - Home & Listings

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -2,14 +2,20 @@
 
 namespace App\Controllers;
 
+use Framework\Database;
+
 class HomeController
 {
+    protected $db;
     public function __construct()
     {
-
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
     }
 
     public function index() {
-        die('HomerController@index');
+        $listings = $this->db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+        loadView('home', ['listings' => $listings]);
     }
 }
