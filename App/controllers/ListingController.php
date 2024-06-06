3e95ae94b0316b3ce73daf5d74d1380cac82598e
Zgrovetin <534477@gmail.com>
2024-06-06 11:26:51 +0200

    Update authorization

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -183,6 +183,12 @@ class ListingController
             return;
         }
 
+        // Authorization
+        if(!Authorization::isOwner($listing->user_id)) {
+            Session::setFlashMessage('error_message', 'You are not allowed to update this listing');
+            return redirect('/listings/' .$listing->id);
+        }
+
         loadView('listings/edit', ['listing' => $listing]);
     }
 
@@ -206,6 +212,12 @@ class ListingController
         if (!$listing) {
             ErrorController::notFound('Listing not found');
             return;
+        }
+
+        // Authorization
+        if(!Authorization::isOwner($listing->user_id)) {
+            Session::setFlashMessage('error_message', 'You are not allowed to update this listing');
+            return redirect('/listings/' .$listing->id);
         }
 
         $allowedFields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
